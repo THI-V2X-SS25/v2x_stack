785fce6cf8f444c39ec827acb9e5d0226d38a2f9
MAtthias Anzinger <anzi.matt@mail.de>
2025-04-16 16:17:56 -0400

    Implement Node for MCM messages

---
+++ src/mc_rx.cpp
@@ -0,0 +1,69 @@
+// #include "mc_message.h"
+#include "mc_rx.h"
+#include <vanetza/btp/ports.hpp>
+
+namespace v2x_stack_btp
+{
+
+CaRxNode::CaRxNode(const rclcpp::NodeOptions & options)
+: Node("mc_rx_node", options)
+{
+}
+
+void CaRxNode::onIndication(msg::BtpDataIndication::ConstSharedPtr indication)
+{
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Indication MC");
+    if (indication->btp_type == msg::BtpDataIndication::BTP_TYPE_B && indication->destination_port == 2001)  // port tbd.
+    {
+        vanetza::asn1::r1::Mcm mcm;
+        const std::vector<unsigned char>& payload = indication->data;
+        const uint8_t* buffer = payload.data();
+
+        auto ok = vanetza::asn1::decode_per((asn_TYPE_descriptor_t&)(asn_DEF_asn_MCM), (void**)(&mcm), (const void*)(buffer), indication->data.size());
+    
+        if (ok)
+        {
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Well decoded MC");
+            publish(mcm);
+        }
+        else
+        {
+            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "ASN.1 decoding failed, drop received MCM");
+        }
+    }
+}
+
+void CaRxNode::publish(const vanetza::asn1::r1::Mcm asn1)
+{
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Entering publish method");
+    std::string error_msg;
+    auto msg = convertMcm(asn1, &error_msg);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Try to Publish MCM");
+
+    if (msg)
+    {
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "There is MCM");
+        node_ = std::make_shared<rclcpp::Node>("mcm_rx");
+        pub_mcm_ = node_->create_publisher<etsi_its_mcm_thi_prima_msgs::msg::MCM>("mcm_received", 20);
+    
+        pub_mcm_->publish(*msg);
+    } else {
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "MCM not correct");
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Error cause: %s", error_msg.c_str());
+    }
+}
+
+} // namespace v2x_stack_btp
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Starting MC RX");
+
+    auto node = std::make_shared<v2x_stack_btp::CaRxNode>(rclcpp::NodeOptions());
+    auto subscription = node->create_subscription<v2x_stack_btp::msg::BtpDataIndication>("btp_data", 20, std::bind(&v2x_stack_btp::CaRxNode::onIndication, node, std::placeholders::_1));
+
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
---
+++ src/mc_rx.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+#include <vanetza/asn1/mcm.hpp>  // currently missing
+#include <v2x_stack_btp/msg/btp_data_indication.hpp>
+#include <etsi_its_mcm_thi_prima_msgs/msg/mcm.hpp>
+#include <cstdint>
+
+namespace v2x_stack_btp
+{
+
+class CaRxNode : public rclcpp::Node
+{
+public:
+    explicit CaRxNode(const rclcpp::NodeOptions & options);
+    void onIndication(const msg::BtpDataIndication::ConstSharedPtr);
+
+private:
+    
+    void publish(const vanetza::asn1::r1::Mcm);
+
+    uint16_t port_;
+    rclcpp::Subscription<msg::BtpDataIndication>::SharedPtr sub_btp_;
+    std::shared_ptr<rclcpp::Publisher<etsi_its_mcm_thi_prima_msgs::msg::MCM>> pub_mcm_;
+    rclcpp::Node::SharedPtr node_;
+};
+
+} // namespace v2x_stack_btp
